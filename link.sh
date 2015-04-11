f3f82a6d586ff2cea79c2ef9b052675eb843f375
gomachan_7 <tt@a-azarashi.jp>
2015-04-12 02:53:15 +0900

    Fix linker script

--- link.sh
+++ link.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim/colors ~/.vim
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/colors ~/.vim
+ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git/gitignore_global ~/.gitignore_global
