7e44a9968a45e6ee52d3d34418edd2582c37b6d5
gomachan_7 <tt@a-azarashi.jp>
2020-07-23 18:09:25 +0900

    Fisher

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,19 @@
 set -x LANG ja_JP.UTF-8
 set -x EDITOR vim
 
+# setup
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 # homebrew
 set -x PATH /usr/local/bin /usr/local/sbin $PATH
 
 # rbenv
 source (rbenv init - | psub)
 set -x PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
 
 # nodebrew
 set -x PATH $HOME/.nodebrew/current/bin $PATH
